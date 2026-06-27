689944405e6b93cda1f6b59a3f450397dd384c32
Ravi Pansheriya <ravipansheriya2612.com>
2026-06-27 17:47:33 +0530

    Prepare frontend for deployment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api', // Change this to your backend URL
+    baseURL: 'import.meta.env.VITE_API_BASE_URL', // Change this to your backend URL
 });
 
 export default API;
